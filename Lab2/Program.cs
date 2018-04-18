c645f407a579ec728f2b660b12b8b1bd3a679898
Mrucznik <mrucznix@gmail.com>
2018-04-18 15:27:56 +0200

    server

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
@@ -24,13 +25,40 @@ namespace Lab2
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Provide communication port: ");
+            if (!int.TryParse(Console.ReadLine(), out var port))
+                port = 7;
+
+
+            Console.WriteLine("Creating socket... ");
             Socket socket = new Socket(
                 AddressFamily.InterNetwork, //serwer działa tylko w wewnętrznej sieci
                 SocketType.Stream,
                 ProtocolType.Unspecified
             );
 
-            socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 3301);
+            Console.WriteLine("Binding... ");
+            socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), port));
+            
+            Console.WriteLine("Listening... ");
+            socket.Listen(5);
+
+            int counter = 0;
+            while (true)
+            {
+                Socket client = socket.Accept();
+                Console.WriteLine("Polaczenie z {0}", client.RemoteEndPoint);
+
+                DateTime now = DateTime.Now;
+                StringBuilder stringBuilder = new StringBuilder();
+                stringBuilder.AppendLine($"Data: {now.Day:00}/{now.Month:00}/{now.Year:0000}");
+                stringBuilder.AppendLine($"Czas: {now.Hour:00}:{now.Minute:00}:{now.Second:00}");
+                stringBuilder.AppendLine($"Jesteś klientem nr {counter}");
+
+                byte[] bufor = Encoding.ASCII.GetBytes(stringBuilder.ToString());
+                client.Send(bufor);
+                client.Close();
+            }
         }
     }
 }
