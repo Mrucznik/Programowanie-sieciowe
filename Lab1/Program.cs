a28efb121f0426e864eec2424478e6bf018a02a2
Mrucznik <mrucznix@gmail.com>
2018-04-24 21:43:35 +0200

    Klient

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,18 @@ namespace Programowanie_sieciowe
     {
         static void Main(string[] args)
         {
+            Socket socket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Unspecified
+            );
+            socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7));
+
+            Console.WriteLine("Sending request");
+            byte[] buffer = new byte[1024];
+            int result = socket.Receive(buffer);
+            String time = Encoding.ASCII.GetString(buffer, 0, result);
+            Console.WriteLine($"RESPONSE: {time}");
         }
     }
 }
