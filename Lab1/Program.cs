d5e98c9ba9c762f69f63644b4d2e44d136481d53
Mrucznik <mrucznix@gmail.com>
2018-05-13 14:50:16 +0200

    Lab1, Lab2, new directories

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -12,23 +12,30 @@ namespace Programowanie_sieciowe
     {
         static void Main(string[] args)
         {
-            Socket socket = new Socket(
-                AddressFamily.InterNetwork,
-                SocketType.Stream,
-                ProtocolType.Unspecified
-            );
-            socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7));
+            try
+            {
+                Socket socket = new Socket(
+                    AddressFamily.InterNetwork,
+                    SocketType.Stream,
+                    ProtocolType.Unspecified
+                );
+                socket.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7));
             
-            Console.WriteLine("Provide request data: ");
-            String data = Console.ReadLine();
-            Console.WriteLine("Sending request");
-            socket.Send(Encoding.ASCII.GetBytes(data));
+                Console.WriteLine("Provide request data: ");
+                String data = Console.ReadLine();
+                Console.WriteLine("Sending request");
+                socket.Send(Encoding.ASCII.GetBytes(data + "\n"));
 
-            Console.WriteLine("Waiting for response");
-            byte[] buffer = new byte[1024];
-            int result = socket.Receive(buffer);
-            String time = Encoding.ASCII.GetString(buffer, 0, result);
-            Console.WriteLine($"RESPONSE: {time}");
+                Console.WriteLine("Waiting for response");
+                byte[] buffer = new byte[1024];
+                int result = socket.Receive(buffer);
+                String response = Encoding.ASCII.GetString(buffer, 0, result);
+                Console.WriteLine($"RESPONSE: {response}.");
+            }
+            catch(SocketException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -27,38 +27,50 @@ namespace Lab2
         {
             Console.WriteLine("Provide communication port: ");
             if (!int.TryParse(Console.ReadLine(), out var port))
-                port = 7;
-
-
-            Console.WriteLine("Creating socket... ");
-            Socket socket = new Socket(
-                AddressFamily.InterNetwork, //serwer działa tylko w wewnętrznej sieci
-                SocketType.Stream,
-                ProtocolType.Unspecified
-            );
-
-            Console.WriteLine("Binding... ");
-            socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), port));
-            
-            Console.WriteLine("Listening... ");
-            socket.Listen(5);
-
-            int counter = 0;
-            while (true)
             {
-                Socket client = socket.Accept();
-                Console.WriteLine("Polaczenie z {0}", client.RemoteEndPoint);
-
-                DateTime now = DateTime.Now;
-                StringBuilder stringBuilder = new StringBuilder();
-                stringBuilder.AppendLine($"Data: {now.Day:00}/{now.Month:00}/{now.Year:0000}");
-                stringBuilder.AppendLine($"Czas: {now.Hour:00}:{now.Minute:00}:{now.Second:00}");
-                stringBuilder.AppendLine($"Jesteś klientem nr {counter}");
-
-                byte[] bufor = Encoding.ASCII.GetBytes(stringBuilder.ToString());
-                client.Send(bufor);
-                client.Close();
+                port = 7;
             }
+
+            try
+            {
+                Console.WriteLine("Creating socket... ");
+
+                Socket socket = new Socket(
+                    AddressFamily.InterNetwork, //serwer działa tylko w wewnętrznej sieci
+                    SocketType.Stream,
+                    ProtocolType.Unspecified
+                );
+
+                Console.WriteLine("Binding... ");
+                socket.Bind(new IPEndPoint(IPAddress.Parse("127.0.0.1"), port));
+
+                Console.WriteLine("Listening... ");
+                socket.Listen(5);
+
+                while (true)
+                {
+                    Socket client = socket.Accept();
+                    Console.WriteLine("Connected with {0}", client.RemoteEndPoint);
+
+                    byte[] buffer = new byte[1024];
+                    client.Receive(buffer);
+
+                    var recivedString = Encoding.ASCII.GetString(buffer);
+                    var sendString = recivedString.Substring(0, recivedString.IndexOf('\n'));
+
+                    Console.WriteLine($"Recived: {recivedString}");
+                    Console.WriteLine($"Sending: {sendString}");
+                    client.Send(Encoding.ASCII.GetBytes(sendString));
+
+                    Console.WriteLine("Data sent, closing connection");
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
         }
     }
 }
