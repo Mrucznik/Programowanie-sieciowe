0686a4ac4aeab91d001a65b220d690cef72d2cde
Mrucznik <mrucznix@gmail.com>
2018-05-16 14:43:05 +0200

    Beginning of multicas servers

--- Lab6/UDPUnicastClient/Program.cs
+++ Lab6/UDPUnicastClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,18 @@ namespace UDPUnicastClient
     {
         static void Main(string[] args)
         {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            EndPoint groupEP = new IPEndPoint(ipAddress, 2222);
+
+            while (true)
+            {
+                byte[] msg = Encoding.UTF8.GetBytes(Console.ReadLine()?.ToCharArray());
+                Console.WriteLine($"Sending {Encoding.UTF8.GetString(msg)}");
+                socket.SendTo(msg, groupEP);
+                socket.ReceiveFrom(msg, ref groupEP);
+                Console.WriteLine($"Recieved {Encoding.UTF8.GetString(msg)}");
+            }
         }
     }
 }
--- Lab6/UDPUnicastServer/Program.cs
+++ Lab6/UDPUnicastServer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,15 @@ namespace UDPUnicastServer
     {
         static void Main(string[] args)
         {
+            UdpClient udpClient = new UdpClient(2222);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, 2222);
+
+            while (true)
+            {
+                byte[] msg = udpClient.Receive(ref groupEP);
+                Console.WriteLine(Encoding.UTF8.GetString(msg));
+                udpClient.Send(msg, msg.Length, groupEP);
+            }
         }
     }
 }
