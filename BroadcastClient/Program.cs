f4a2852eee1ffea7b6656bad67e1d407436cc59c
Mrucznik <mrucznix@gmail.com>
2018-05-16 15:23:33 +0200

    Broadcast server and client

--- BroadcastClient/Program.cs
+++ BroadcastClient/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,25 @@ namespace BroadcastClient
     {
         static void Main(string[] args)
         {
+            UdpClient client = new UdpClient { ExclusiveAddressUse = false };
+
+            IPEndPoint localEp = new IPEndPoint(IPAddress.Any, 2222);
+
+            client.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            client.ExclusiveAddressUse = false;
+
+            client.Client.Bind(localEp);
+            
+            client.EnableBroadcast = true;
+
+            Console.WriteLine("Listening this will never quit so you will need to ctrl-c it");
+
+            while (true)
+            {
+                Byte[] data = client.Receive(ref localEp);
+                string strData = Encoding.Unicode.GetString(data);
+                Console.WriteLine(strData);
+            }
         }
     }
 }
--- BroadcastServer/Program.cs
+++ BroadcastServer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,24 @@ namespace BroadcastServer
     {
         static void Main(string[] args)
         {
+            UdpClient udpclient = new UdpClient();
+
+            udpclient.EnableBroadcast = true;
+
+            IPEndPoint remoteep = new IPEndPoint(IPAddress.Broadcast, 2222);
+
+            Console.WriteLine("Press ENTER to start sending messages");
+            Console.ReadLine();
+
+            for (int i = 0; i <= 8000; i++)
+            {
+                var buffer = Encoding.Unicode.GetBytes(i.ToString());
+                udpclient.Send(buffer, buffer.Length, remoteep);
+                Console.WriteLine("Sent " + i);
+            }
+
+            Console.WriteLine("All Done! Press ENTER to quit.");
+            Console.ReadLine();
         }
     }
 }
