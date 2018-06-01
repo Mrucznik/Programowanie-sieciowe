8c7527364c9b91f8170ea02c8c779eee43c3fc4e
Mrucznik <mrucznix@gmail.com>
2018-06-01 17:36:58 +0200

    Zadanie 1 - testowy servlet

--- Servlety/src/Servlet.java
+++ Servlety/src/Servlet.java
@@ -4,14 +4,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "Servlet")
+
+@WebServlet(name = "Servlet", urlPatterns = { "/servlet" })
 public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        processRequest(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    {
+        response.setContentType("text/html;carset=UTF-8");
+        try {
+            PrintWriter out = response.getWriter();
+
+            out.println("<h1>No elo</h1>");
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- Servlety/web/index.jsp
+++ Servlety/web/index.jsp
@@ -7,10 +7,10 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
+<head>
     <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+</head>
+<body>
+    <h4><a href="servlet">Click here to go to the serverlet</a></h4>
+</body>
 </html>
